75a9ac5d7d854798081e190f6172ad26e530113f
setbr1 <dcdm2mil@gmail.com>
2026-04-21 14:58:46 -0300

    feat(backend): ativando o cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.enableCors({
+    origin: 'http://localhost:3001', // A porta onde seu Next.js está rodando
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Importante se formos usar cookies no futuro
+  });
+  
   // Ativando a validação global
   app.useGlobalPipes(
     new ValidationPipe({
